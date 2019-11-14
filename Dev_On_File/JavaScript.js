172db211cad219815d34606c741c31e4fbaaa82d
Julian <ferrarinijulian@gmail.com>
2019-11-14 16:45:59 +0100

    Update LvL
    Add button
    next update -> lvl2 , lvl3, lvl4
    bug -> enter multiple lauch game

--- Dev_On_File/JavaScript.js
+++ Dev_On_File/JavaScript.js
@@ -119,96 +119,7 @@ function pausefunc(){
   }
 }
 
-class Voiture {
-  constructor(x, y, ctx, taille) {
-    this.x = x;
-    this.y = y;
-    this.dx = 0;
-    this.ctx = ctx;
-    this.taille = taille;
-    this.ctx.fillStyle = this.color;  
-    this.ctx.lineWith = 10;
-    this.pv = 10;
-    this.score = 0;
 
-  }
-  
-  draw() {
-	ctx.save();
-
-    ctx.fillStyle = "black";
-    //Roue haut gauche
-    ctx.fillRect(this.x, this.y, 30*this.taille, 40*this.taille);
-    //Roue haut droite
-    ctx.fillRect(this.x+70*this.taille, this.y, 30*this.taille, 40*this.taille);
-    //Entre roue haut
-    ctx.fillRect(this.x+25*this.taille, this.y+15*this.taille, 50*this.taille, 10*this.taille);
-    
-    //Roue bas gauche
-    ctx.fillRect(this.x, this.y+100*this.taille, 30*this.taille, 40*this.taille);
-    //Roue bas droite
-    ctx.fillRect(this.x+70*this.taille, this.y+100*this.taille, 30*this.taille, 40*this.taille);
-    //Entre roue bas
-    ctx.fillRect(this.x+25*this.taille, this.y+115*this.taille, 50*this.taille, 10*this.taille);
-    
-    //Centre
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.x+35*this.taille, this.y, 30*this.taille, 140*this.taille);
-    
-    //triangle droit
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(this.x+65*this.taille,this.y+50*this.taille);
-    ctx.lineTo(this.x+65*this.taille,this.y+100*this.taille);
-    ctx.lineTo(this.x+90*this.taille,this.y+75*this.taille);
-    ctx.fill();
-    
-    //triangle gauche
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(this.x+35*this.taille,this.y+50*this.taille);
-    ctx.lineTo(this.x+35*this.taille,this.y+100*this.taille);
-    ctx.lineTo(this.x+10*this.taille,this.y+75*this.taille);
-    ctx.fill();
-    
-    //protege roue bas gauche
-    ctx.fillStyle = "grey";
-    ctx.fillRect(this.x+5*this.taille, this.y+105*this.taille, 15*this.taille, 30*this.taille);
-    
-    //protege roue bas droit
-    ctx.fillStyle = "grey";
-    ctx.fillRect(this.x+80*this.taille, this.y+105*this.taille, 15*this.taille, 30*this.taille);
-    
-    //protege roue bas droit
-    ctx.fillStyle = "darkgrey";
-    ctx.fillRect(this.x+20*this.taille, this.y+110*this.taille, 60*this.taille, 20*this.taille);
-    ctx.restore();
-    
-    
-
-  }
-  move() {
-    
-  this.x += this.dx;
-  this.score += 1;
-  // j'ai pris un peu plus large que la route  
-  if(this.x + 10/this.taille >= route[108].xd ) {
-      this.pv -= 0.1;
-      ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-      ctx.fillRect(0,0,canvas.width, canvas.height);
-      
-  }
-  //document.getElementById("Canvas").style.background = "url('images/terrain.jpg') ";
-  if(this.x - 3/this.taille <= route[108].xg ){
-      this.pv -= 0.1;
-      ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-      ctx.fillRect(0,0,canvas.width, canvas.height);
-  }
-  
-  
-  }
-
-}
 /*
 function myFunction() {
   document.getElementById("demo").innerHTML = "Hello World";
@@ -222,20 +133,7 @@ function init() {
   h = canvas.height;
   //context graphique
   ctx = canvas.getContext("2d");
-  ctx.fillStyle = "white";
-  ctx.fillRect(0,0,600,600);
-  ctx.font = "30pt Arial Black";
-  ctx.fillStyle = "blue";
-  ctx.strokeStyle = "black";
-  ctx.fillText("Pressez ENTRER", 120, 290);
-  ctx.strokeText("Pressez ENTRER", 120, 290);
-  ctx.fillText("pour commencer", 120, 340);
-  ctx.strokeText("pour commencer", 120, 340);
-  creerTableauRoute();
-  creerTableauChemin();
-  creerTableauSol();
-  
-   
+  Affichage();   
   tableau = document.querySelector("#Tableau");
   w1 = tableau.width;
   h1 = tableau.height;
